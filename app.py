1153224a783174f504b5f88b9c15efb49e2a8854
mariadalid <20243rd014@utez.edu.mx>
2025-10-17 15:56:03 -0600

    feat: Importación y configuración basica

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 #Bahena Praxedis Maria Adalid
 #Estrada Romero Angeles
 #Dario Emiliano 
+
+#Importacion y conf basica
+from flask import Flask, render_template, request, session
+import random
+
+app = Flask(__name__)
+app.secret_key = 'clave_secreta'
