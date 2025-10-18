522e64d7292f95a0f756cf046214c21863f8bdc2
mariadalid <20243rd014@utez.edu.mx>
2025-10-17 22:02:51 -0600

    feat: Creación de la applicación Flask

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+#Fernando
 #Bahena Praxedis Maria Adalid
 #Estrada Romero Angeles
-#Dario Emiliano 
+#Emiliano
+#Jesus
+#Josh
 
 #Importacion y conf basica
 from flask import Flask, render_template, request, session
 import random
 
+#Creación de la Aplicación Flask y definición de una llave secreta 
 app = Flask(__name__)
 app.secret_key = 'clave_secreta'
+
